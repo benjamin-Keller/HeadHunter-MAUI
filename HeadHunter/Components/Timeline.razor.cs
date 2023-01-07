af5f4dbf8a0712e6c95e2feef8b6752d401bebee
Benjamin Keller <zhero240@gmail.com>
2023-01-07 23:00:20 +0200

    Adds event timeline page

--- HeadHunter/Components/Timeline.razor.cs
+++ HeadHunter/Components/Timeline.razor.cs
@@ -5,9 +5,11 @@ namespace HeadHunter.Components;
 
 public partial class Timeline
 {
-    [Parameter]
-    public string Title { get; set; }
+    [Parameter] public string Title { get; set; }
 
-    [Parameter]
-    public List<EventData> Events { get; set; }
+    [Parameter] public List<EventData> Events { get; set; }
+
+    [Parameter] public EventCallback<EventData> OnEventSelected { get; set; }
+
+    public async Task OnEventClick(EventData selectedEvent) => await OnEventSelected.InvokeAsync(selectedEvent);
 }
--- HeadHunter/Pages/Events.razor.cs
+++ HeadHunter/Pages/Events.razor.cs
@@ -9,9 +9,12 @@ public partial class Events
     [Inject] private EventsService eventsService { get; set; }
 
     private EventsModel EventsData { get; set; } = new();
+    private EventData EventData { get; set; } = default;
 
     protected override async Task OnInitializedAsync()
     {
         EventsData = await eventsService.GetEventsAsync();
     }
+
+    private void selectedEvent(EventData selectedEvent) => EventData = selectedEvent;
 }
